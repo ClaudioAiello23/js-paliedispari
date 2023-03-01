96289b9da9eff50baafe8b07810c8e5b764918f2
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-01 16:58:25 +0100

    PARI-DISPARI - funzione num PARI + vincitore

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -2,20 +2,19 @@
 
 // l'utente sceglie PARI o DISPARI
 let pariDispari = prompt('scrivi PARI o DISPARI');
-console.log('Scelta utente: ', pariDispari);
+console.log('Scelta Pari/Dispari: ', pariDispari);
 
-const evenNumber = [2, 4];
-const oddNumber = [1, 3, 5];
+const evenArray = [2, 4];
+const oddArray = [1, 3, 5];
 
 
 // l'utente sceglie un numero da 1 a 5
 let userNumber = Number (prompt('scrivi un numero da 1 a 5'));
-console.log('Scelta utente: ', userNumber);
+console.log('Scelta Numero Utente: ', userNumber);
 
 
 // FUNZIONE GENERAZIONE NUMERO RANDOM DA 1 A 5
-function GenRandomNum() {
-
+function genRandomNum() {
 
     const randomNumber = Math.floor((Math.random() * 5) + 1);
     return randomNumber;
@@ -23,17 +22,33 @@ function GenRandomNum() {
 }
 
 // RICHIAMO FUNZIONE GENERAZIONE NUMERO RANDOM E LA STAMPO
-let cpuNumber = GenRandomNum();
-console.log ('scelta cpu: ', cpuNumber);
+let cpuNumber = genRandomNum();
+console.log ('Scelta cpu: ', cpuNumber);
 
 
 // SOMMA DEL NUMERO UTENTE + NUMERO RANDOM CPU
 let finalNumber = userNumber + cpuNumber;
-console.log (finalNumber);
+console.log ('Somma Numeri: ', finalNumber);
 
 
+// FUNZIONE CHE STABILISCE SE UN NUMERO è PARI
+function resultEven(num) {
+   
+    let result = (num % 2 === 0);
+    return result;
 
+}
 
+// C0NFRONTO FINALE - RICHIAMO FIUNZIONE - CHI VINCE TRA PARI E DISPARI
+if (finalNumber = resultEven(finalNumber)) {
+
+    console.log('HA VINTO PARI');
+    
+} else{
+    console.log('HA VINTO DISPARI');
+    
+
+}
 
 
 
