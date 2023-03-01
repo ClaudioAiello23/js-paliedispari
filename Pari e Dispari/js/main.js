0f1543cab0810f33662d0377b98790792e8bd736
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-01 15:52:03 +0100

    PARI-DISPARI: somma numero user + numero cpu

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -4,23 +4,34 @@
 let pariDispari = prompt('scrivi PARI o DISPARI');
 console.log('Scelta utente: ', pariDispari);
 
-// l'utente sceglie un numvero da 1 a 5
-let numberChoice = prompt('scrivi un numero da 1 a 5');
-console.log('Scelta utente: ', numberChoice);
+const evenNumber = [2, 4];
+const oddNumber = [1, 3, 5];
 
 
-// FUNZIONE GENERAZIONE NUMERO RANDOM 
+// l'utente sceglie un numero da 1 a 5
+let userNumber = Number (prompt('scrivi un numero da 1 a 5'));
+console.log('Scelta utente: ', userNumber);
+
+
+// FUNZIONE GENERAZIONE NUMERO RANDOM DA 1 A 5
 function GenRandomNum() {
+
 
     const randomNumber = Math.floor((Math.random() * 5) + 1);
     return randomNumber;
     
 }
 
-
-// RICHIAMO FUNZIONE GENERAZIONE NUMERO RANDOM 
+// RICHIAMO FUNZIONE GENERAZIONE NUMERO RANDOM E LA STAMPO
 let cpuNumber = GenRandomNum();
 console.log ('scelta cpu: ', cpuNumber);
+
+
+// SOMMA DEL NUMERO UTENTE + NUMERO RANDOM CPU
+let finalNumber = userNumber + cpuNumber;
+console.log (finalNumber);
+
+
 
 
 
