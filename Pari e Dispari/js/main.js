10cb16b24912e357aeceeecb47579f15d6bf96c1
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-01 15:09:24 +0100

    PARI-DISPARI: inserimento dati utente -

---
+++ Pari e Dispari/js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// l'utente sceglie PARI o DISPARI
+let pariDispari = prompt('scrivi PARI o DISPARI');
+console.log('Scelta utente: ', pariDispari);
+
+// l'utente sceglie un numvero da 1 a 5
+let numberChoice = prompt('scrivi un numero da 1 a 5');
+console.log('Scelta utente: ', numberChoice);
+
+
+/*PROVA IMPOSTAZIONE PER SCELTA PARI DISPARI
+
+    if (pariDispari === 'PARI' || pariDispari === 'DISPARI' ) {
+
+       let numberChoice = prompt('scrivi un numero da 1 a 5');
+        console.log('Scelta utente: ', numberChoice);
+        
+    } else {
+
+       pariDispari = prompt('scrivi PARI o DISPARI');
+    }
+
+    console.log('Scelta utente: ', pariDispari);*/
+    
+
+
+/*PROVA IMPOSTAZIONE PER SCELTA NUMERO DA 1 A 5
+
+let numberChoice = prompt('scrivi un numero da 1 a 5');
+
+   if (numberChoice > 1 && numberChoice <= 5) {
+
+        console.log('Scelta utente: ', numberChoice);
+        
+    } else {
+    
+    numberChoice = prompt('scrivi un numero da 1 a 5');
+}
+*/
+
+
+
+
+
+    
+ 
+
+
+
