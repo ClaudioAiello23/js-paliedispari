8b63587828f20d3a010868fb20889933ceaa55e8
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-01 19:19:07 +0100

    PALINDROMA: creazioni variabili e ciclo for

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/* creare una userWord che corrisponde all'inserimento della parola
+ da parte dell'utente (prompt)*/
+let userWord = prompt('inserisci la parola');
+console.log (userWord);
+
+/* inizializzare una variabile che consideri la lettera singola
+e che gestirò nel ciclo For*/
+let singleLetter;
+
+/* creazione di un ciclo For che legga tutta la parola e che consideri
+la variabile singleLetter come singola lettera della parola; il ciclo
+si completa quando l'indice di posizionamento arriva all'ultima lettera*/
+ for (let i = 0; i < userWord.length; i++) {
+
+    singleLetter = userWord [i];
+    console.log(singleLetter);
+    
+ }
+
+
+
+
+
+
